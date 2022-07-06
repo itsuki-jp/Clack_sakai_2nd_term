5c7dbd1752e5151b6e6a1dcfd8bcd11e1ebc26bb
itsuki-jp <itsuki19601@gmail.com>
2022-07-06 22:33:54 +0900

    stop_(玉、自機動く)

--- shooting_game/index.js
+++ shooting_game/index.js
@@ -4,6 +4,7 @@ const shooter = {
     x: 100,
     y: 100,
 };
+let beams = [];
 
 function init() {
     const shooterElm = document.createElement("img");
@@ -15,7 +16,36 @@ function init() {
     playArea.appendChild(shooterElm);
 }
 
-function move(event) {
+function addBeam() {
+    let beam = { src: "images/ball.png" };
+    beam.x = shooter.x;
+    beam.y = shooter.y;
+    const beamElm = document.createElement("img");
+    beamElm.src = beam.src;
+    beamElm.setAttribute("class", "beam");
+    beamElm.style.top = beam.x + "px";
+    beamElm.style.left = beam.y + "px";
+    beam.beamElm = beamElm;
+    beams.push(beam);
+    playArea.appendChild(beamElm);
+
+}
+
+function moveBeam() {
+    if (beams.length === 0) { return; }
+    let newBeams = []
+    for (let beam of beams) {
+        if (beam.y < 0) { beam.beamElm.remove() }
+        beam.y -= 1;
+        beam.beamElm.style.left = beam.y + "px";
+        newBeams.push(beam);
+    }
+    beams = newBeams;
+}
+
+function moveShooter(event) {
+    let isShooterMoved = true;
+    let isProjecttileNewBullet;
     if (event.code === "ArrowUp") {
         shooter.x -= 10;
     } else if (event.code === "ArrowDown") {
@@ -24,9 +54,19 @@ function move(event) {
         shooter.y -= 10;
     } else if (event.code === "ArrowRight") {
         shooter.y += 10;
+    } else {
+        isShooterMoved = false;
+        isProjecttileNewBullet = event.code === "Space" ? true : false;
     }
-    shooter.shooterElm.style.top = shooter.x + "px";
-    shooter.shooterElm.style.left = shooter.y + "px";
+    if (isShooterMoved) {
+        shooter.shooterElm.style.top = shooter.x + "px";
+        shooter.shooterElm.style.left = shooter.y + "px";
+    } else if (isProjecttileNewBullet) { addBeam(); }
+
 }
-document.addEventListener("keydown", move);
+
+document.addEventListener("keydown", moveShooter);
 init()
+setInterval(() => {
+    moveBeam();
+}, 10);
