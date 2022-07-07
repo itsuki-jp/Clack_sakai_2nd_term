3b57b5fc4576b00178ad71854fd0454a96c9818b
itsuki-jp <itsuki19601@gmail.com>
2022-07-07 19:36:56 +0900

    飽きた

--- shooting_game/index.js
+++ shooting_game/index.js
@@ -1,72 +1,154 @@
 const playArea = document.getElementById("playArea");
-const shooter = {
-    src: "images/shooter.png",
-    x: 100,
-    y: 100,
-};
-let beams = [];
+const mainArea = { H: 700, W: 700 }
+
+class ObjClass {
+    constructor(y, x, h, w) {
+        this.src;
+        this.y = y;
+        this.x = x;
+        this.h = h;
+        this.w = w;
+        this.elm;
+    }
+    createElm(className) {
+        const elm = document.createElement("img");
+        elm.src = this.src;
+        elm.setAttribute("class", className)
+        elm.style.left = this.x + "px";
+        elm.style.top = this.y + "px";
+
+        this.elm = elm;
+    }
+}
+
+class ShooterClass extends ObjClass {
+    constructor(y, x) {
+        super(y, x, 80, 100);
+        this.src = "images/shooter.png";
+        this.beams = [];
+    }
+    moveBeams() {
+        if (this.beams.length === 0) { return; }
+        let newBeams = [];
+        for (let beam of this.beams) {
+            if (beam.y < -beam.h || mainArea.H < beam.y || beam.x < -beam.w || mainArea.W < beam.x) {
+                beam.elm.remove();
+                continue;
+            }
+            beam.y += beam.dy;
+            beam.x += beam.dx;
+            beam.elm.style.top = beam.y + "px";
+            beam.elm.style.left = beam.x + "px";
+            newBeams.push(beam);
+        }
+        this.beams = newBeams;
+    }
+
+    addBeam(playArea) {
+        for (let angle = -2; angle < 3; angle++) {
+            let beam = new BeamClass(this.y + 25, this.x, angle / 5, -1, 0)
+            beam.createElm("beam");
+            playArea.appendChild(beam.elm);
+            this.beams.push(beam);
+        }
+    }
+}
+
+class BeamClass extends ObjClass {
+    constructor(y, x, dy, dx, theta) {
+        super(y, x, 25, 25);
+        this.src = "images/ball.png";
+        this.dy = dy;
+        this.dx = dx;
+        this.theta = theta;
+    }
+}
+
+class EnemyClass extends ObjClass {
+    constructor() {
+        let y = Math.random() * (mainArea.H - 80);
+        let x = Math.random() * (mainArea.W / 4);
+        super(y, x, 100, 80);
+        this.src = "images/enemy.png";
+        this.beams = [];
+        this.timeCount = 0;
+        this.interval = 10;
+    }
+    moveBeams() {
+        if (this.beams.length === 0) { return; }
+        let newBeams = [];
+        for (let beam of this.beams) {
+            if (beam.y < -beam.h || mainArea.H < beam.y || beam.x < -beam.w || mainArea.W < beam.x) {
+                beam.elm.remove();
+                continue;
+            }
+            beam.y += beam.dy;
+            beam.x += beam.dx;
+            beam.elm.style.top = beam.y + "px";
+            beam.elm.style.left = beam.x + "px";
+            newBeams.push(beam);
+        }
+        this.beams = newBeams;
+    }
+    addNormalBeam(playArea) {
+        for (let angle = -2; angle < 3; angle++) {
+            let beam = new BeamClass(this.y + this.h / 2, this.x + this.w / 2, angle / 5, 1, 0);
+            beam.createElm("beam");
+            playArea.appendChild(beam.elm);
+            this.beams.push(beam);
+        }
+    }
+}
+
+let shooter;
+let enemies = [];
 
 function init() {
-    const shooterElm = document.createElement("img");
-    shooterElm.src = shooter.src;
-    shooterElm.setAttribute("id", "shooter")
-    shooterElm.style.top = shooter.x + "px";
-    shooterElm.style.left = shooter.y + "px";
-    shooter.shooterElm = shooterElm;
-    playArea.appendChild(shooterElm);
+    shooter = new ShooterClass(700 - 80, 700 - 100, );
+    shooter.createElm("shooter");
+    playArea.appendChild(shooter.elm);
 }
 
-function addBeam() {
-    let beam = { src: "images/ball.png" };
-    beam.x = shooter.x;
-    beam.y = shooter.y;
-    const beamElm = document.createElement("img");
-    beamElm.src = beam.src;
-    beamElm.setAttribute("class", "beam");
-    beamElm.style.top = beam.x + "px";
-    beamElm.style.left = beam.y + "px";
-    beam.beamElm = beamElm;
-    beams.push(beam);
-    playArea.appendChild(beamElm);
-
+function addEnemy() {
+    let enemy = new EnemyClass();
+    enemy.addNormalBeam(playArea);
+    enemy.createElm("enemy");
+    playArea.appendChild(enemy.elm);
+    enemies.push(enemy);
 }
 
-function moveBeam() {
-    if (beams.length === 0) { return; }
-    let newBeams = []
-    for (let beam of beams) {
-        if (beam.y < 0) { beam.beamElm.remove() }
-        beam.y -= 1;
-        beam.beamElm.style.left = beam.y + "px";
-        newBeams.push(beam);
-    }
-    beams = newBeams;
-}
-
-function moveShooter(event) {
+function keyDownEvents(event) {
     let isShooterMoved = true;
     let isProjecttileNewBullet;
     if (event.code === "ArrowUp") {
-        shooter.x -= 10;
-    } else if (event.code === "ArrowDown") {
-        shooter.x += 10;
-    } else if (event.code === "ArrowLeft") {
         shooter.y -= 10;
-    } else if (event.code === "ArrowRight") {
+    } else if (event.code === "ArrowDown") {
         shooter.y += 10;
+    } else if (event.code === "ArrowLeft") {
+        shooter.x -= 10;
+    } else if (event.code === "ArrowRight") {
+        shooter.x += 10;
     } else {
         isShooterMoved = false;
         isProjecttileNewBullet = event.code === "Space" ? true : false;
     }
+
     if (isShooterMoved) {
-        shooter.shooterElm.style.top = shooter.x + "px";
-        shooter.shooterElm.style.left = shooter.y + "px";
-    } else if (isProjecttileNewBullet) { addBeam(); }
+        shooter.elm.style.left = shooter.x + "px";
+        shooter.elm.style.top = shooter.y + "px";
+    } else if (isProjecttileNewBullet) { shooter.addBeam(playArea); }
+
+    if (event.code === "KeyZ") {
+        addEnemy();
+    }
 
 }
 
-document.addEventListener("keydown", moveShooter);
-init()
+document.addEventListener("keydown", keyDownEvents);
+init();
 setInterval(() => {
-    moveBeam();
+    shooter.moveBeams();
+    for (let elm of enemies) {
+        elm.moveBeams();
+    }
 }, 10);
