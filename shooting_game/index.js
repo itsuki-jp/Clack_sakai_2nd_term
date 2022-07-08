4b4985a3361863f080cc208d7afe828a3b47ca70
itsuki-jp <itsuki19601@gmail.com>
2022-07-08 11:46:44 +0900

    とりま

--- shooting_game/index.js
+++ shooting_game/index.js
@@ -1,5 +1,8 @@
 const playArea = document.getElementById("playArea");
-const mainArea = { H: 700, W: 700 }
+const mainArea = { H: 700, W: 700 };
+const shooterSize = { H: 80, W: 100 };
+const enemySize = { H: 100, W: 80 };
+const beamSize = { H: 25, W: 25 };
 
 class ObjClass {
     constructor(y, x, h, w) {
@@ -19,14 +22,6 @@ class ObjClass {
 
         this.elm = elm;
     }
-}
-
-class ShooterClass extends ObjClass {
-    constructor(y, x) {
-        super(y, x, 80, 100);
-        this.src = "images/shooter.png";
-        this.beams = [];
-    }
     moveBeams() {
         if (this.beams.length === 0) { return; }
         let newBeams = [];
@@ -42,6 +37,39 @@ class ShooterClass extends ObjClass {
             newBeams.push(beam);
         }
         this.beams = newBeams;
+    }
+    collisionDetection(obj2) {
+        let obj1 = this;
+        let obj1Centre = { x: obj1.x + obj1.w / 2, y: obj1.y + obj1.h / 2 };
+        let obj2Centre = { x: obj2.x + obj2.w / 2, y: obj2.y + obj2.h / 2 };
+        let distX = Math.abs(obj1Centre.x - obj2Centre.x);
+        let distY = Math.abs(obj1Centre.y - obj2Centre.y);
+        if (distX <= (obj1.w + obj2.w) / 2 && distY <= (obj1.h + obj2.h) / 2) {
+            console.log("hit");
+            return true;
+        }
+        return false;
+    }
+    collisionDetectionWithArr(arr, both = false) {
+        let newArr = [];
+        let deleteSelf = false;
+        for (let obj of arr) {
+            if (this.collisionDetection(obj)) {
+                obj.elm.remove();
+                deleteSelf = true;
+                continue;
+            }
+            newArr.push(obj);
+        }
+        if (both) { return deleteSelf; }
+    }
+}
+
+class ShooterClass extends ObjClass {
+    constructor(y, x) {
+        super(y, x, 80, 100);
+        this.src = "images/shooter.png";
+        this.beams = [];
     }
 
     addBeam(playArea) {
@@ -71,29 +99,18 @@ class EnemyClass extends ObjClass {
         super(y, x, 100, 80);
         this.src = "images/enemy.png";
         this.beams = [];
+        this.deleted = false;
     }
-    moveBeams() {
-        if (this.beams.length === 0) { return; }
-        let newBeams = [];
-        for (let beam of this.beams) {
-            if (beam.y < -beam.h || mainArea.H < beam.y || beam.x < -beam.w || mainArea.W < beam.x) {
-                beam.elm.remove();
-                continue;
-            }
-            beam.y += beam.dy;
-            beam.x += beam.dx;
-            beam.elm.style.top = beam.y + "px";
-            beam.elm.style.left = beam.x + "px";
-            newBeams.push(beam);
-        }
-        this.beams = newBeams;
+    addBeam(beam, playArea) {
+        beam.createElm("beam");
+        playArea.appendChild(beam.elm);
+        this.beams.push(beam);
+
     }
     addNormalBeam(playArea) {
         for (let angle = -2; angle < 3; angle++) {
             let beam = new BeamClass(this.y + this.h / 2, this.x + this.w / 2, angle / 5, 1, 0, "images/enemyBullet.png");
-            beam.createElm("beam");
-            playArea.appendChild(beam.elm);
-            this.beams.push(beam);
+            this.addBeam(beam, playArea);
         }
     }
     addCircularBeam(playArea) {
@@ -103,9 +120,7 @@ class EnemyClass extends ObjClass {
                 this.y + this.h / 2, this.x + this.w / 2 - 12.5,
                 2 * Math.sin(angle * (2 * Math.PI) / numOfBullet), 2 * Math.cos(angle * (2 * Math.PI) / numOfBullet), 0,
                 "images/enemyBullet.png");
-            beam.createElm("beam");
-            playArea.appendChild(beam.elm);
-            this.beams.push(beam);
+            this.addBeam(beam, playArea);
         }
     }
     addCircularBeam2(playArea) {
@@ -115,15 +130,14 @@ class EnemyClass extends ObjClass {
         let angle = 0;
         let timeCount = 0;
         let temp = setInterval(() => {
-            if (count == 10) { clearInterval(temp); }
+            if (this.deleted) { clearInterval(temp); }
+            if (count == 20) { clearInterval(temp); }
             if (timeCount === interval) {
                 let beam = new BeamClass(
                     this.y + this.h / 2, this.x + this.w / 2 - 12.5,
                     Math.sin(angle * (2 * Math.PI) / numOfBullet), Math.cos(angle * (2 * Math.PI) / numOfBullet), 0,
                     "images/enemyBullet.png");
-                beam.createElm("beam");
-                playArea.appendChild(beam.elm);
-                this.beams.push(beam);
+                this.addBeam(beam, playArea);
                 timeCount = 0;
                 angle++;
                 if (angle % numOfBullet === 0) { count++; }
@@ -143,12 +157,25 @@ function init() {
 
 function addEnemy() {
     let enemy = new EnemyClass();
-    //enemy.addNormalBeam(playArea);
-    //enemy.addCircularBeam(playArea);
     enemy.addCircularBeam2(playArea);
     enemy.createElm("enemy");
     playArea.appendChild(enemy.elm);
     enemies.push(enemy);
+}
+
+function collisionDetectionWithEnemyBullet() {
+    for (let enemy of enemies) {
+        shooter.collisionDetectionWithArr(enemy.beams);
+    }
+}
+
+function collisionDetectionWithShooterBullet() {
+    for (let enemy of enemies) {
+        if (enemy.collisionDetectionWithArr(shooter.beams, true)) {
+            enemy.elm.remove();
+            enemy.deleted = true;
+        }
+    }
 }
 
 function keyDownEvents(event) {
@@ -181,8 +208,19 @@ function keyDownEvents(event) {
 document.addEventListener("keydown", keyDownEvents);
 init();
 setInterval(() => {
+    collisionDetectionWithEnemyBullet();
+    collisionDetectionWithShooterBullet();
     shooter.moveBeams();
-    for (let elm of enemies) {
+    let rmv = [];
+    for (let i = 0; i < enemies.length; i++) {
+        let elm = enemies[i];
+        if (elm.deleted && elm.beams.length === 0) {
+            rmv.push(i);
+            continue;
+        }
         elm.moveBeams();
+    }
+    for (let i = 0; i < rmv.length; i++) {
+        enemies.splice(rmv[i] - i, 1);
     }
 }, 10);
